1c2c35f0142f056dc36f257c01dca1ff04a60fa9
Jim Grant <jimgrant@gmail.com>
2015-07-20 16:41:42 -0700

    add ImagerProfile model

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ImagerProfile(models.Model):
+    PHOTO_TYPES = (
+        ('N', 'Nature Photography'),
+        ('P', 'Paranormal Photography'),
+        ('A', 'Aerial Photography'),
+        ('B', 'Black and White Photography'),
+        ('F', 'Forensic Photography')
+    )
+    user = models.OneToOneField(User)
+    fav_camera = models.CharField(max_length=256)
+    address = models.CharField(max_length=256)
+    url = models.URLField()
+    photo_type = models.CharField(max_length=1,
+                                  choices=PHOTO_TYPES,
+                                  default='N')
+    active = models.BooleanField()
+
+    @property
+    def is_active(self):
+        return self.active
