40dac0608e77e65187c519caeb1eb83aaf7e528b
Jim Grant <jimgrant@gmail.com>
2015-07-20 17:03:03 -0700

    add app to settings. make user field unique in ImagerProfile. begin work on post save and delete signals.

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save, post_delete
 
 
 class ImagerProfile(models.Model):
@@ -10,7 +11,7 @@ class ImagerProfile(models.Model):
         ('B', 'Black and White Photography'),
         ('F', 'Forensic Photography')
     )
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, unique=True)
     fav_camera = models.CharField(max_length=256)
     address = models.CharField(max_length=256)
     url = models.URLField()
@@ -28,3 +29,15 @@ class ImagerProfile(models.Model):
     @property
     def is_active(self):
         return self.active
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        ImagerProfile.objects.create(user=instance)
+
+
+def delete_user_profile(sender, instance, created, **kwargs):
+    pass
+
+post_save.connect(create_user_profile, sender=User)
+post_delete.connect(delete_user_profile, sender=User)
