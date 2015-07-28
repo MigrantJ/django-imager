a8c8db2c49156fe308f788c533a4f85ad40ca516
Jim Grant <jimgrant@gmail.com>
2015-07-28 09:33:30 -0700

    change get() to get_context_data()

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -6,9 +6,8 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-    def get(self, request, *args, **kwargs):
+    def get_context_data(self, **kwargs):
         p = Photos.objects.filter(published='public').order_by('?').first()
-        context = {
-            'photo': p
-        }
-        return render(request, self.template_name, context=context)
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['photo'] = p
+        return context
