36130f9e05e4b327714b9064e4c9a5fbf870ea41
Jim Grant <jimgrant@gmail.com>
2015-07-28 15:58:30 -0700

    TextIndexView logs in. Registration view tests

--- imagersite/imagersite/tests.py
+++ imagersite/imagersite/tests.py
@@ -1,22 +1,19 @@
 from django.test import TestCase
 from django.test import Client
-# from django.contrib.auth.models import User
-# import factory
-
-
-# class UserFactory(factory.DjangoModelFactory):
-#     class Meta:
-#         model = User
-#
-#     username = 'person'
-#     password = 'secret'
+from django.contrib.auth.models import User
 
 
 class TestIndexView(TestCase):
 
-    def setUp(self):
-        self.c = Client()
-        self.res = self.c.get('/')
+    @classmethod
+    def setUpTestData(cls):
+        cls.c = Client()
+        cls.res = cls.c.get('/')
+        cls.password = 'password'
+        cls.testuser = User.objects.create_user(
+            username='person',
+            password=cls.password
+        )
 
     def test_responds(self):
         assert self.res.status_code == 200
@@ -27,9 +24,35 @@ class TestIndexView(TestCase):
     def test_links_on_not_auth(self):
         assert 'id="no_auth"' in self.res.content
 
-    # def test_links_on_auth(self):
-    #     print self.testuser.password
-    #     assert self.c.login(username=self.testuser.username, password=self.testuser.password)
+    def test_links_on_auth(self):
+        assert self.c.login(username=self.testuser.username, password='password')
+        self.res = self.c.get('/')
+        assert 'id="auth"' in self.res.content
 
-    def tearDown(self):
-        self.res = None
+    @classmethod
+    def tearDownClass(cls):
+        cls.c = None
+        cls.res = None
+        cls.password = None
+        cls.testuser = None
+
+
+class TestRegistrationView(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.c = Client()
+        cls.res = cls.c.get('/accounts/register/')
+
+    def test_responds(self):
+        assert self.res.status_code == 200
+
+    def test_form_is_present(self):
+        assert self.res.context['form'] is not None
+        assert 'login_form flex' in self.res.content
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.c = None
+        cls.res = None
+
