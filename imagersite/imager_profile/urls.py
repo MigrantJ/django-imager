7c09d0eecf93a98e367a6ebfc225cc169b1331ae
Scott Schmidt <sjschmidt44@gmail.com>
2015-07-28 17:19:14 -0700

    Create profile, library, and album views for imager site.

--- imagersite/imager_profile/urls.py
+++ imagersite/imager_profile/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.IndexView.as_view(), name='profile'),
+    url(r'^images/library', views.PhotoListView.as_view(), name='library'),
+    url(r'^images/albums', views.AlbumListView.as_view(), name='albums'),
+    url(r'^images/albums/', views.AlbumDetailListView.as_view(), name='albums_detail'),
 ]
--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,16 +1,37 @@
-from django.shortcuts import render
-from imager_images.models import Photos
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from imager_images.models import Photos, Album
 
 
 class IndexView(TemplateView):
     template_name = 'profile.html'
 
     def get_context_data(self, **kwargs):
-        p = Photos.objects.filter(published='public').order_by('?').first()
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['photo'] = p
         return context
 
-# This is for getting the user pref fields on the profile page via context.
-# user.profile._meta.get_fields()
+
+class PhotoListView(ListView):
+    template_name = 'photos_list.html'
+    model = Photos
+
+    def get_context_data(self, **kwargs):
+        context = super(ListView, self).get_context_data(**kwargs)
+        return context
+
+
+class AlbumListView(ListView):
+    template_name = 'album_list.html'
+    model = Album
+
+    def get_context_data(self, **kwargs):
+        context = super(ListView, self).get_context_data(**kwargs)
+        return context
+
+
+class AlbumDetailListView(ListView):
+    template_name = 'photos_list.html'
+    model = Album
+
+    def get_context_data(self, **kwargs):
+        context = super(ListView, self).get_context_data(**kwargs)
+        return context
