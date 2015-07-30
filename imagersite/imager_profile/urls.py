428bc4d528c6a295e4d5d5f3d4e1764ba07490e7
Scott Schmidt <sjschmidt44@gmail.com>
2015-07-30 08:57:56 -0700

    Update style and views to show photo and album detail.

--- imagersite/imager_profile/urls.py
+++ imagersite/imager_profile/urls.py
@@ -3,8 +3,8 @@ import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='profile'),
-    url(r'^images/library$', views.PhotoListView.as_view(), name='library'),
-    url(r'^images/albums$', views.AlbumListView.as_view(), name='albums'),
+    url(r'^images/library', views.PhotoListView.as_view(), name='library'),
+    url(r'^images/albums', views.AlbumListView.as_view(), name='albums'),
     url(
         r'^images/albums/(?P<pk>\d+)$',
         views.AlbumDetailListView.as_view(),
