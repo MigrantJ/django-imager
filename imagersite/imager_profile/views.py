9ca3cf1d592c383afdb5a471f5a98c4089c62233
Scott Schmidt <sjschmidt44@gmail.com>
2015-07-29 18:17:22 -0700

    Update views to include querysets rather than context data.

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -4,10 +4,13 @@ from imager_images.models import Photos, Album
 
 class IndexView(TemplateView):
     template_name = 'profile.html'
-    
+
     def get_context_data(self, **kwargs):
-        pho = Photos.objects.filter(published='public').order_by('?').first()
-        alb = Album.objects.all().order_by('?').first()
+        pho = Photos.objects.filter(
+            published='public',
+            user=self.request.user).order_by('?').first()
+        alb = Album.objects.filter(
+            user=self.request.user).order_by('?').first()
         context = super(IndexView, self).get_context_data(**kwargs)
         context['photo'] = pho
         context['album'] = alb
@@ -18,24 +21,25 @@ class PhotoListView(ListView):
     template_name = 'photos_list.html'
     model = Photos
 
-    def get_context_data(self, **kwargs):
-        context = super(ListView, self).get_context_data(**kwargs)
-        return context
+    def get_queryset(self):
+        return Photos.objects.filter(
+            published='public',
+            user=self.request.user)
 
 
 class AlbumListView(ListView):
     template_name = 'album_list.html'
     model = Album
 
-    def get_context_data(self, **kwargs):
-        context = super(ListView, self).get_context_data(**kwargs)
-        return context
+    def get_queryset(self):
+        return Album.objects.filter(user=self.request.user)
 
 
 class AlbumDetailListView(ListView):
     template_name = 'photos_list.html'
     model = Album
 
-    def get_context_data(self, **kwargs):
-        context = super(ListView, self).get_context_data(**kwargs)
-        return context
+    def get_queryset(self):
+        return Photos.objects.filter(
+            published='public',
+            user=self.request.user)
